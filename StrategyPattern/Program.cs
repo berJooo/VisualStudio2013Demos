f1c2f5d466ce4580ce8d0e0c12ea9f202b6e9144
朱博文 <805808189@qq.com>
2017-09-12 16:34:06 +0800

    完成StrategyPattern

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace StrategyPattern {
     class Program {
         static void Main(string[] args) {
-            MallardDuck md = new MallardDuck();
-            RedheadDuck rd = new RedheadDuck();
-            RubberDuck rud = new RubberDuck();
-
-            md.Display();
-            rd.Display();
-            rud.Quack();
-            rud.Fly();
+            Duck modeDuck = new ModeDuck();
+            modeDuck.performFly();
+            modeDuck.SetFlyBehavior(new FlyRocket());
+            modeDuck.SetQuackBehavior(new Squeak());
+            modeDuck.performFly();
+            modeDuck.performQuack();
+            modeDuck.Swim();
+            modeDuck.Display();
         }
     }
 
@@ -22,11 +22,10 @@ namespace StrategyPattern {
     public abstract class Duck {
 
         /// <summary>
-        /// 鸭子叫
+        /// 声明变量类型为行为的接口类型
         /// </summary>
-        public virtual void Quack() {
-            Console.WriteLine("呱呱");
-        }
+        public QuackBehavior quackBehavior;//每只鸭子都会引用实现QuackBehavior接口的对象
+        public FlyBehavior flyBehavior;
 
         /// <summary>
         /// 鸭子游泳
@@ -41,12 +40,26 @@ namespace StrategyPattern {
         public abstract void Display();
 
         /// <summary>
-        /// 鸭子飞行
+        /// 此方法取代了Quack
         /// </summary>
-        public virtual void Fly() {
-            Console.WriteLine("飞行");
+        public void performQuack() {
+            quackBehavior.Quack1();//鸭子对象不亲自处理呱呱叫行为，而是委托给quackBehavior引用的对象
+        }
+        
+        /// <summary>
+        /// 此方法取代了Fly().同理Quack()方法
+        /// </summary>
+        public void performFly() {
+            flyBehavior.Fly();
         }
 
+        public void SetFlyBehavior(FlyBehavior fb) {
+            flyBehavior = fb;
+        }
+
+        public void SetQuackBehavior(QuackBehavior qb) {
+            quackBehavior = qb;
+        }
     }
 
 
@@ -54,23 +67,94 @@ namespace StrategyPattern {
         public override void Display() {
             throw new NotImplementedException();
         }
-
-        public override void Quack() {
-            //base.Quack();
-            Console.WriteLine("吱吱");
-        }
-
-        //方案1，将Duck中的Fly方法为虚，橡皮鸭覆盖掉此方法并不做任何事情
-        public override void Fly() {
-            //base.Fly();
-        }
     }
 
 
     class MallardDuck : Duck {
+
+        /// <summary>
+        /// 写一个构造函数，可以保证创建对象时，将实例复制给父类的quackBehavior
+        /// </summary>
+        public MallardDuck() {
+            quackBehavior = new Quack();//绿头鸭使用Quack类处理呱呱叫，所以当performQuack()被调用时，叫的职责被委托给Quack对象，而我们得到了真正的呱呱叫。
+            flyBehavior = new FlyWithWings();//使用FlyWithWings作为其FlyBehavior类型。
+        }
+
         public override void Display() {
             //throw new NotImplementedException();
             Console.WriteLine("绿头");
+        }       
+    }
+
+
+    /// <summary>
+    /// 这是一个接口，所有飞行类都实现它，所有新的飞行类必须实现Fly方法
+    /// </summary>
+    public interface FlyBehavior {
+        void Fly();
+    }
+
+    /// <summary>
+    /// 呱呱叫行为同样，一个接口只包含一个需要实现的Quack方法
+    /// </summary>
+    public interface QuackBehavior {
+        void Quack1();
+    }
+
+    /// <summary>
+    /// 这个类实现了所有有翅膀的鸭子飞行动作
+    /// </summary>
+    class FlyWithWings : FlyBehavior {
+
+        public void Fly() {
+            Console.WriteLine("会飞");
+        }
+    }
+
+    /// <summary>
+    /// 实现了所有不会飞的鸭子的动作
+    /// </summary>
+    class FlyNoWay : FlyBehavior {
+
+        public void Fly() {
+            Console.WriteLine("不会飞");
+        }
+    }
+
+
+    class FlyRocket : FlyBehavior {
+        public void Fly() {
+            Console.Write("火箭动力推进装置");
+        }
+    }
+
+    /// <summary>
+    /// 呱呱叫
+    /// </summary>
+    class Quack : QuackBehavior {
+
+        public void Quack1() {
+            Console.WriteLine("呱呱叫");
+        }
+    }
+
+    /// <summary>
+    /// 名为呱呱叫，实则吱吱叫
+    /// </summary>
+    class Squeak : QuackBehavior {
+
+        public void Quack1() {
+            Console.WriteLine("吱吱叫");
+        }
+    }
+
+    /// <summary>
+    /// 名为呱呱叫，实则不出声
+    /// </summary>
+    class MuteQuack : QuackBehavior {
+
+        public void Quack1() {
+            Console.WriteLine("不会叫");
         }
     }
 
@@ -79,6 +163,17 @@ namespace StrategyPattern {
         public override void Display() {
             //throw new NotImplementedException();
             Console.WriteLine("红头");
+        }
+    }
+
+    class ModeDuck : Duck {
+        public ModeDuck() {
+            flyBehavior = new FlyNoWay();
+            quackBehavior = new Quack();
+        }
+
+        public override void Display() {
+            Console.WriteLine("我就是一木头鸭子");
         }
     }
 }
