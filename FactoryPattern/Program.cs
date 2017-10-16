19e384460baa7e6225fa8cca4c4902347775dc07
朱博文 <805808189@qq.com>
2017-10-16 15:47:58 +0800

    更新FactoryPattern

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * 工厂设计模式
+ */
+
 namespace FactoryPattern {
+
+    abstract class Pizza {
+        public abstract void Prepare();
+        public abstract void Bake();
+        public abstract void Cut();
+        public abstract void Box();
+    }
+
+    class CheesePizza : Pizza {    }
+
+    class GreekPizza : Pizza {    }
+
+    class PepperoniPizza : Pizza {    }
+
+    class SimplePizzaFactory : Pizza {    }
+
+    class PizzaStore {
+
+        public Pizza pizza;
+
+        //Pizza订单
+        public Pizza OrderPizza(string type) {
+            if(type.Equals("cheese")) {
+                pizza = new CheesePizza();
+            } else if(type.Equals("greek")) {
+                pizza = new GreekPizza();
+            } else if(type.Equals("pepperoni")) {
+                pizza = new PepperoniPizza();
+            }
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+
+            return pizza;
+        }
+    }
+
     class Program {
         static void Main(string[] args) {
         }
